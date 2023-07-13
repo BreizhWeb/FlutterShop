6507cff0ff21e0f522f08648fdfda9de40394d15
Dorian Haupas <dorian.haupas@live.fr>
2023-07-14 00:55:16 +0200

    feat(account): apply API for account

--- lib/account_screen.dart
+++ lib/account_screen.dart
@@ -1,6 +1,32 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends StatefulWidget {
+  @override
+  _AccountScreenState createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  Map<String, dynamic> userData = {};
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    final response = await http.get(Uri.parse('https://dummyjson.com/users/1'));
+    if (response.statusCode == 200) {
+      setState(() {
+        userData = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to fetch user data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,156 +35,168 @@ class AccountScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: 300,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('../../assets/pp.jpg'),
-                      fit: BoxFit.cover,
+        child: userData.isNotEmpty
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(userData['image']),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        'Mon compte',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Prénom',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                Spacer(),
-                Text(
-                  'Mon compte',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                  TextFormField(
+                    initialValue: userData['firstName'],
+                    decoration: InputDecoration(
+                      hintText: userData['firstName'],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Prénom',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'John',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Nom',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Doe',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Pseudo',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Jodo',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Téléphone',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: '+33102030405',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Email',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'john.doe@domain.com',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Adresse',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: '42 rue des escargots',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Nom',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    initialValue: userData['lastName'],
+                    decoration: InputDecoration(
+                      hintText: userData['lastName'],
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Pseudo',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    initialValue: userData['username'],
+                    decoration: InputDecoration(
+                      hintText: userData['username'],
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Téléphone',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    initialValue: userData['phone'],
+                    decoration: InputDecoration(
+                      hintText: userData['phone'],
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Email',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    initialValue: userData['email'],
+                    decoration: InputDecoration(
+                      hintText: userData['email'],
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Adresse',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    initialValue: userData['address']['address'],
+                    decoration: InputDecoration(
+                      hintText: userData['address']['address'],
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Code postal',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Code postal',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextFormField(
+                              initialValue: userData['address']['postalCode'],
+                              decoration: InputDecoration(
+                                hintText: userData['address']['postalCode'],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: '42420',
+                      SizedBox(width: 16.0),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Ville',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextFormField(
+                              initialValue: userData['address']['city'],
+                              decoration: InputDecoration(
+                                hintText: userData['address']['city'],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-                SizedBox(width: 16.0),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Ville',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Lorette',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
     );
   }
