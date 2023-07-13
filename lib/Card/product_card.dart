dc8640e108bf833bcee3a863441ea73bf657dfec
Dorian Haupas <dorian.haupas@live.fr>
2023-07-14 00:55:05 +0200

    feat(shop): apply API for shop

--- lib/Card/product_card.dart
+++ lib/Card/product_card.dart
@@ -1,73 +1,88 @@
-import 'package:flutter/material.dart';  
-  
-class ProductCard extends StatelessWidget {  
-  const ProductCard({super.key});  
-  
-  @override  
-  Widget build(BuildContext context) {  
-    return Card(  
-      child: Column(  
-        crossAxisAlignment: CrossAxisAlignment.start,  
-        children: <Widget>[  
-          const ClipRRect(  
-            borderRadius: BorderRadius.only(  
-              topLeft: Radius.circular(8),  
-              topRight: Radius.circular(8),  
-            ),  
-            child: Image(  
-              image: NetworkImage(  
-                  'https://i.dummyjson.com/data/products/5/1.jpg'),  
-            ),  
-          ),  
-          Container(  
-            padding: EdgeInsets.all(6),  
-            child: const Text('Catégorie', textAlign: TextAlign.left),  
-          ),  
-          Container(  
-            padding: EdgeInsets.all(6),  
-            child: const Text('Nom du produit qui peut parfois être un peu long',  
-                style : TextStyle(  
-                  fontWeight: FontWeight.bold,  
-                  fontSize: 18,  
-                )),  
-          ),  
-          Container(  
-            padding: EdgeInsets.all(6),  
-            child: Row(  
-              children: const [  
-                Icon(Icons.star),  
-                Icon(Icons.star),  
-                Icon(Icons.star),  
-                Icon(Icons.star),  
-                Icon(Icons.star_outline),  
-              ],  
-            ),  
-          ),  
-          Container(  
-            padding: EdgeInsets.all(6),  
-            child: const Text('Description du produit qui peut faire un certain nombre de lignes alors autant lui laisser un peu de place', textAlign: TextAlign.left),  
-          ),  
-          Container(  
-            padding: EdgeInsets.all(10),  
-            child : Row(  
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,  
-              children: [  
-                const Text('49,00 €', style: TextStyle(  
-                  fontWeight: FontWeight.bold,  
-                  fontSize: 16  
-                ),),  
-              IconButton(  
-                icon: const Icon(Icons.add_shopping_cart),  
-                tooltip: 'Add to cart',  
-                onPressed: () {  
-  
-                },  
-              ),  
-              ],  
-            )  
-          )  
-        ],  
-      )  
-    );  
-  }  
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ProductCard extends StatelessWidget {
+  final dynamic product;
+
+  const ProductCard({required this.product, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(8),
+              topRight: Radius.circular(8),
+            ),
+            child: Image.network(
+              product['thumbnail'],
+              fit: BoxFit.cover,
+              width: double.infinity, 
+              height: 200, 
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(6),
+            child: Text(
+              product['category'],
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(6),
+            child: Text(
+              product['title'],
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(6),
+            child: Row(
+              children: List.generate(
+                5,
+                (index) => Icon(
+                  index < product['rating'] ? Icons.star : Icons.star_outline,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(6),
+            child: Text(
+              product['description'],
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '${product['price']} €',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add_shopping_cart),
+                  tooltip: 'Add to cart',
+                  onPressed: () {
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/shop_screen.dart
+++ lib/shop_screen.dart
@@ -1,25 +1,54 @@
-import 'package:flutter/material.dart';  
-import 'package:flutterapp/Card/product_card.dart';  
-  
-class ShopScreen extends StatelessWidget {  
-  @override  
-  Widget build(BuildContext context) {  
-  
-    return Center(  
-      child: GridView.builder(  
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(  
-          crossAxisCount: 3,  
-          crossAxisSpacing: 10,  
-          mainAxisSpacing: 10,  
-          childAspectRatio: 0.9,  
-        ),  
-        primary: false,  
-        itemCount: 12,  
-        padding: const EdgeInsets.all(10),  
-          itemBuilder: (BuildContext context, int index) {  
-            return ProductCard();  
-          }  
-      ),  
-      );  
-  }  
+import 'package:flutter/material.dart';
+import 'package:flutterapp/Card/product_card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ShopScreen extends StatefulWidget {
+  @override
+  _ShopScreenState createState() => _ShopScreenState();
+}
+
+class _ShopScreenState extends State<ShopScreen> {
+  Set<dynamic> products = {};
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProducts();
+  }
+
+  Future<void> fetchProducts() async {
+    final response = await http.get(Uri.parse('https://dummyjson.com/products'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        products = Set.from(data['products']);
+      });
+    } else {
+      throw Exception('Failed to fetch products');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio: 1.2,
+        ),
+        primary: false,
+        itemCount: products.length,
+        padding: const EdgeInsets.all(10),
+        itemBuilder: (BuildContext context, int index) {
+          final product = products.toList()[index];
+
+          return ProductCard(product: product);
+        },
+      ),
+    );
+  }
 }
